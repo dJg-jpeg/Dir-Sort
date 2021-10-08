18ca13cdb947a3fbc966fae622f1f6ce09efbe97
Volodymyr <vvl5656@gmail.com>
2021-10-08 22:04:12 +0300

    normalize and get_cmd_args functions

--- dir_sort.py
+++ dir_sort.py
@@ -1,2 +1,40 @@
+from pathlib import Path
+import os
+import sys
+import re
+
+
+def get_cmd_args():
+    if len(sys.argv) != 2:
+        print("Insert only 1 argument")
+    else:
+        return sys.argv[1]
+
+
+def get_path_and_extension(filename):
+    index = filename.find('.')
+    return filename[:index], filename[index + 1:]
+
+
+def normalize(name):
+    table_symbols = ('абвгґдеєжзиіїйклмнопрстуфхцчшщюяыэАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯЫЭьъЬЪ',
+                     (
+                         *u'abvhgde', 'ye', 'zh', *u'zyi', 'yi', *u'yklmnoprstuf', 'kh', 'ts',
+                         'ch', 'sh', 'shch', 'yu', 'ya', 'y', 'ye', *u'ABVHGDE', 'Ye', 'Zh', *u'ZYI',
+                         'Yi', *u'YKLMNOPRSTUF', 'KH', 'TS', 'CH', 'SH', 'SHCH', 'YU', 'YA', 'Y', 'YE',
+                         *(u'_' * 4)
+                     )
+                     )
+    map_cyr_to_latin = {ord(src): dest for src, dest in zip(*table_symbols)}
+    print(map_cyr_to_latin)
+    rx = re.compile(r"[^\w_]")
+    return rx.sub('_', name.translate(map_cyr_to_latin))
+
+
 if __name__ == '__main__':
-    pass
+    p = Path(get_cmd_args())
+    if p.is_dir():
+        # do something
+        pass
+    else:
+        print('It is not a directory , please insert a valid directory path')
