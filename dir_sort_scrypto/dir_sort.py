23ccde33790c0529d24c3f5c343966f2be0b13a7
Volodymyr <vvl5656@gmail.com>
2021-10-13 21:39:23 +0300

    optimized code and get file extension fix

--- dir_sort_scrypto/dir_sort.py
+++ dir_sort_scrypto/dir_sort.py
@@ -2,6 +2,14 @@ from pathlib import Path
 import sys
 import re
 import shutil
+FOLDERS_NAMES = ('image', 'video', 'audio', 'document', 'archive', 'unknown')
+FILE_TYPES_EXTENSIONS = (
+                         ('.jpeg', '.jpg', '.png', '.svg'),
+                         ('.avi', '.mp4', '.mov', '.mkv'),
+                         ('.mp3', '.ogg', '.wav', '.amr'),
+                         ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'),
+                         ('.zip', '.gz', '.tar')
+                        )
 
 
 def get_cmd_args():
@@ -12,79 +20,30 @@ def get_cmd_args():
 
 
 def check_file_extension(extension):
-    if extension == 'jpeg' or extension == 'jpg' or extension == 'png' or extension == 'svg':
-        return 'image'
-    elif extension == 'avi' or extension == 'mp4' or extension == 'mov' or extension == 'mkv':
-        return 'video'
-    elif extension == 'mp3' or extension == 'ogg' or extension == 'wav' or extension == 'amr':
-        return 'audio'
-    elif (extension == 'doc' or extension == 'docx' or extension == 'txt' or extension == 'pdf'
-          or extension == 'xlsx' or extension == 'pptx'):
-        return 'document'
-    elif extension == 'zip' or extension == 'gz' or extension == 'tar':
-        return 'archive'
-    return 'unknown'
+    for file_types in FILE_TYPES_EXTENSIONS:
+        if extension in file_types:
+            return FOLDERS_NAMES[FILE_TYPES_EXTENSIONS.index(file_types)]
+    return FOLDERS_NAMES[5]
 
 
-def find_all_files(path, images, videos, documents, audios, archives, unknown):
+def find_all_files(path, files):
     for content in path:
-        if content.is_dir() and (content.name == 'images' or
-                                 content.name == 'documents' or
-                                 content.name == 'video' or
-                                 content.name == 'audio' or
-                                 content.name == 'archives'):
-            continue
-        elif content.is_dir():
-            inside_dirs = find_all_files(content.iterdir(), images, videos, documents, audios, archives, unknown)
-            images = inside_dirs[0]
-            videos = inside_dirs[1]
-            audios = inside_dirs[2]
-            documents = inside_dirs[3]
-            archives = inside_dirs[4]
-            unknown = inside_dirs[5]
-        else:
-            extension = get_filename_and_extension(content.name)[1]
-            if check_file_extension(extension) == 'image':
-                images.append(content)
-            elif check_file_extension(extension) == 'video':
-                videos.append(content)
-            elif check_file_extension(extension) == 'audio':
-                audios.append(content)
-            elif check_file_extension(extension) == 'document':
-                documents.append(content)
-            elif check_file_extension(extension) == 'archive':
-                archives.append(content)
-            else:
-                unknown.append(content)
-    return images, videos, audios, documents, archives, unknown
+        if content.is_file():
+            extension = check_file_extension(get_filename_and_extension(content)[1])
+            files[FOLDERS_NAMES.index(extension)].append(content)
+        elif content.name not in FOLDERS_NAMES:
+            inside_dirs = find_all_files(content.iterdir(), files)
+            files = inside_dirs
+    return files
 
 
 def make_dirs(path):
-    images = check_is_dir_exist(path / 'images')
-    videos = check_is_dir_exist(path / 'videos')
-    audios = check_is_dir_exist(path / 'audios')
-    documents = check_is_dir_exist(path / 'documents')
-    archives = check_is_dir_exist(path / 'archives')
-    unknown = check_is_dir_exist(path / 'unknown')
-    if images is False:
-        (path / 'images').mkdir()
-        images = check_is_dir_exist(path / 'images')
-    if videos is False:
-        (path / 'videos').mkdir()
-        videos = check_is_dir_exist(path / 'videos')
-    if audios is False:
-        (path / 'audios').mkdir()
-        audios = check_is_dir_exist(path / 'audios')
-    if documents is False:
-        (path / 'documents').mkdir()
-        documents = check_is_dir_exist(path / 'documents')
-    if archives is False:
-        (path / 'archives').mkdir()
-        archives = check_is_dir_exist(path / 'archives')
-    if unknown is False:
-        (path / 'unknown').mkdir()
-        unknown = check_is_dir_exist(path / 'unknown')
-    return images, videos, audios, documents, archives, unknown
+    sort_folders = []
+    for folder_name in FOLDERS_NAMES:
+        if check_is_dir_exist(path / folder_name) is False:
+            ((path / folder_name).mkdir())
+        sort_folders.append(check_is_dir_exist(path / folder_name))
+    return sort_folders
 
 
 def check_is_dir_exist(path):
@@ -94,28 +53,19 @@ def check_is_dir_exist(path):
 
 
 def move_files(files, dirs):
-    for image in files[0]:
-        image.replace(dirs[0] / image.name)
-        files[0][files[0].index(image)] = dirs[0] / image.name
-    for video in files[1]:
-        video.replace(dirs[1] / video.name)
-        files[1][files[1].index(video)] = dirs[1] / video.name
-    for audio in files[2]:
-        audio.replace(dirs[2] / audio.name)
-        files[2][files[2].index(audio)] = dirs[2] / audio.name
-    for document in files[3]:
-        document.replace(dirs[3] / document.name)
-        files[3][files[3].index(document)] = dirs[3] / document.name
+    for file_type in files:
+        for file_path in file_type:
+            file_path.replace(dirs[files.index(file_type)] / file_path.name)
+            files[files.index(file_type)][files[files.index(file_type)].index(file_path)] = \
+                dirs[files.index(file_type)] / file_path.name
     for archive in files[4]:
-        (dirs[4] / get_filename_and_extension(archive.name)[0]).mkdir()
-        archive.replace(dirs[4] / get_filename_and_extension(archive.name)[0] / archive.name)
-        files[4][files[4].index(archive)] = dirs[4] / get_filename_and_extension(archive.name)[0]
-        archive_path = dirs[4] / get_filename_and_extension(archive.name)[0] / archive.name
-        archive_dir = dirs[4] / get_filename_and_extension(archive.name)[0]
-        unpack_archives(archive_path, archive_dir)
-    for unknown in files[5]:
-        unknown.replace(dirs[5] / unknown.name)
-        files[5][files[5].index(unknown)] = dirs[5] / unknown.name
+        path_to_archive_dir = dirs[4] / get_filename_and_extension(archive)[0]
+        archive_path = path_to_archive_dir / archive.name
+        path_to_archive_dir.mkdir()
+        archive.replace(archive_path)
+        files[4][files[4].index(archive)] = path_to_archive_dir
+        unpack_archives(archive_path, path_to_archive_dir)
+        archive_path.unlink()
     return files
 
 
@@ -124,15 +74,13 @@ def unpack_archives(archive, new_dir_path):
 
 
 def get_filename_and_extension(filename):
-    index = filename.find('.')
-    return filename[:index], filename[index + 1:]
+    return filename.resolve().stem, filename.suffix
 
 
 def rename_files(files):
     for category in files:
         for content in category:
-            filename = (normalize(get_filename_and_extension(content.name)[0]) +
-                        '.' + get_filename_and_extension(content.name)[1])
+            filename = normalize(get_filename_and_extension(content)[0]) + get_filename_and_extension(content)[1]
             content.rename(content.parent / filename)
             files[files.index(category)][category.index(content)] = content.parent / filename
     return files
@@ -165,7 +113,7 @@ def normalize(name):
 if __name__ == '__main__':
     p = Path(get_cmd_args())
     if p.is_dir():
-        all_files = find_all_files(p.iterdir(), [], [], [], [], [], [])
+        all_files = find_all_files(p.iterdir(), [[], [], [], [], [], []])
         all_files = rename_files(all_files)
         new_dirs = make_dirs(p)
         new_files = move_files(all_files, new_dirs)
